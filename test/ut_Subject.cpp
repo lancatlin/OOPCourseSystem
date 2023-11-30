3499fdf125ebf37aa1a40dd323d38a8d8487dc7a
Justin Lin <wancat@wancat.cc>
2023-11-30 23:06:24 +0800

    feat: init RequiredSubject as Subject

--- test/ut_Subject.cpp
+++ test/ut_Subject.cpp
@@ -2,6 +2,7 @@
 #include "Subject.hpp"
 #include <gtest/gtest.h>
 #include <memory>
+#include <set>
 
 TEST(Subject, InitRequiredSubjectSuccess) {
   RequiredSubject s("TestCourse", {1, 2, 3});
@@ -9,4 +10,15 @@ TEST(Subject, InitRequiredSubjectSuccess) {
   // std::make_shared<RequiredSubject>("TestCourse", {12, 23, 45});
   ASSERT_EQ(s.GetTime(), std::set({1, 2, 3}));
   ASSERT_EQ(s.GetName(), "TestCourse");
+}
+
+TEST(Subject, InitRequiredSubjectAsSubject) {
+  std::shared_ptr<Subject> s =
+      std::make_shared<RequiredSubject>("TestCourse", std::set({1, 2, 3}));
+
+  // This doesn't work:
+  // std::shared_ptr<Subject> s =
+  //     std::make_shared<RequiredSubject>("TestCourse", {1, 2, 3});
+  ASSERT_EQ(s->GetTime(), std::set({1, 2, 3}));
+  ASSERT_EQ(s->GetName(), "TestCourse");
 }
